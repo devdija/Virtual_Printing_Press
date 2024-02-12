8d6f66bcd062f1d6dc7239dc36b01698cd9ac317
Adavize123 <adavizeabdullahi@gmail.com>
2024-02-12 17:49:14 +0100

    new updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Virtual_Printing_Press
 A virtual printing press system
+# https://github.com/devdija/Virtual_Printing_Press
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
+  screens: {
+    xs: '450px',
+    sm: '640px',
+    md: '768px',
+    xmd: '900px',
+    lg: '1025px',
+    xl: '1280px',
+    '2xl': '1536px',
+    '3xl': '1800px',
+  },
 }
-
